0876fc7c763f4ddc61ab31627b5742d2139a1980
Gourang-dev <77156496+Gourang-dev@users.noreply.github.com>
2021-06-14 19:26:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
 song1 = "";
 song2 = "";
 
-song1_status = "";
-song2_status = "";
-
-scoreRightWrist = 0;
-scoreLeftWrist = 0;
-
 rightWristX = 0;
 rightWristY = 0;
 
@@ -39,9 +33,6 @@ function gotPoses(results)
   if(results.length > 0)
   {
 	console.log(results);
-	scoreRightWrist =  results[0].pose.keypoints[10].score;
-	scoreLeftWrist =  results[0].pose.keypoints[9].score;
-	console.log("scoreRightWrist = " + scoreRightWrist + "scoreLeftWrist = " + scoreLeftWrist);
 	
 	rightWristX = results[0].pose.rightWrist.x;
 	rightWristY = results[0].pose.rightWrist.y;
@@ -56,50 +47,4 @@ function gotPoses(results)
 
 function draw() {
 	image(video, 0, 0, 600, 500);
-	
-	song1_status = song1.isPlaying();
-	song2_status = song2.isPlaying();
-
-	fill("#FF0000");
-	stroke("#FF0000");
-
-	if(scoreRightWrist > 0.2)
-	{ 
-		circle(rightWristX,rightWristY,20);
-
-			song2.stop();
-
-		if(song1_status == false)
-		{
-			song1.play();
-			document.getElementById("song").innerHTML = "Playing - Harry Potter Theme Song"
-		}
-	}
-
-	if(scoreLeftWrist > 0.2)
-	{
-		circle(leftWristX,leftWristY,20);
-
-			song1.stop();
-
-		if(song2_status == false)
-		{
-			song2.play();
-			document.getElementById("song").innerHTML = "Playing - Peter Pan Song"
-		}
-	}
-
 }
-
-function play()
-{
-	song.play();
-	song.setVolume(1);
-	song.rate(1);
-}
-
-
-
-
-
-
